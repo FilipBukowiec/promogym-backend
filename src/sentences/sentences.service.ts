2bc75a58b1c784d6cc1db23efdd3d36b996f7799
Filip Bukowiec <filip.bukowiec@ad.ruckzuck.pl>
2025-08-12 15:31:44 +0200

    add all sentences service

--- src/sentences/sentences.service.ts
+++ src/sentences/sentences.service.ts
@@ -22,8 +22,6 @@ export class SentencesService {
     return createSentence.save();
   }
 
-
-
   async createMany(sentencesDto: CreateSentenceDto[]): Promise<Sentence[]> {
     if (sentencesDto.length === 0) {
       throw new Error('the list is empty');
@@ -39,13 +37,9 @@ export class SentencesService {
     return this.sentenceModel.insertMany(sentencesToInsert);
   }
 
-
-
   async getAllSentences(): Promise<Sentence[]> {
     return this.sentenceModel.find().sort({ order: 1 }).lean();
   }
-
-
 
   async getSentenceOfTheDay(): Promise<Sentence> {
     const now = new Date();
@@ -74,8 +68,6 @@ export class SentencesService {
     return sentence;
   }
 
-  
-
   async deleteById(sentenceId: string): Promise<void> {
     const sentence = await this.sentenceModel.findById(sentenceId);
     if (!sentence) {
@@ -90,5 +82,118 @@ export class SentencesService {
       { order: { $gt: orderToMove } },
       { $inc: { order: -1 } },
     );
+  }
+
+  async deleteAllSentences(): Promise<void> {
+    const session = await this.sentenceModel.db.startSession();
+    session.startTransaction();
+
+    try {
+      await this.sentenceModel.deleteMany({}).session(session);
+      await session.commitTransaction();
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    } finally {
+      session.endSession();
+    }
+  }
+
+  async moveUp(id: string): Promise<void> {
+    const session = await this.sentenceModel.db.startSession();
+    session.startTransaction();
+
+    try {
+      const currentSentence = await this.sentenceModel
+        .findById(id)
+        .session(session);
+
+      if (!currentSentence) {
+        throw new Error('Sentencja nie znaleziona');
+      }
+
+      if (currentSentence.order === 1) {
+        throw new Error('sentencja na samej górze');
+      }
+
+      const aboveSentence = await this.sentenceModel
+        .findOne({ order: currentSentence.order - 1 })
+        .session(session);
+      if (!aboveSentence) {
+        throw new Error('Nie znaleziono elementu powyżej');
+      }
+
+      await this.sentenceModel
+        .updateOne(
+          { _id: currentSentence._id },
+          { order: currentSentence.order - 1 },
+        )
+        .session(session);
+
+      await this.sentenceModel
+        .updateOne(
+          { _id: aboveSentence._id },
+          { order: aboveSentence.order + 1 },
+        )
+        .session(session);
+
+      await session.commitTransaction();
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    } finally {
+      session.endSession();
+    }
+  }
+
+  async moveDown(id: string): Promise<void> {
+    const session = await this.sentenceModel.db.startSession();
+    session.startTransaction();
+
+    try {
+      const currentSentence = await this.sentenceModel
+        .findById(id)
+        .session(session);
+
+      if (!currentSentence) {
+        throw new Error('Sentencja nie znaleziona');
+      }
+
+      const totalSentences = await this.sentenceModel
+        .countDocuments()
+        .session(session);
+
+      if (currentSentence.order === totalSentences) {
+        throw new Error('sentencja już jest na dole');
+      }
+
+      const underSentence = await this.sentenceModel
+        .findOne({ order: currentSentence.order + 1 })
+        .session(session);
+      if (!underSentence) {
+        throw new Error('Nie znaleziono elementu poniżej');
+      }
+
+      await this.sentenceModel
+        .updateOne(
+          { _id: currentSentence._id },
+          { order: currentSentence.order + 1 },
+        )
+        .session(session);
+
+      await this.sentenceModel
+        .updateOne(
+          { _id: underSentence._id },
+          { order: underSentence.order - 1 },
+        )
+        .session(session);
+
+      await session.commitTransaction();
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    } finally {
+      session.endSession();
+    }
   }
 }
