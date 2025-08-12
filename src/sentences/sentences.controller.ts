ec7778c1ad34968b6483606029ae1a23edcbf225
unknown <filip.bukowiec@interia.pl>
2025-08-12 22:51:02 +0200

    create all module sentences

--- src/sentences/sentences.controller.ts
+++ src/sentences/sentences.controller.ts
@@ -1,16 +1,53 @@
-import { Controller, Get, Post, UseGuards, Headers } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Body, Delete, Param, Put } from '@nestjs/common';
 import { SentencesService } from './sentences.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Sentence } from './sentence.model';
+import { CreateSentenceDto } from './create-sentence.dto';
 
 @Controller('sentences')
+@UseGuards(AuthGuard('jwt'))
+
 export class SentencesController {
   constructor(private readonly sentencesServices: SentencesService) {}
 
-@UseGuards(AuthGuard('jwt'))
 @Get()
-async findAll(@Headers('tenant-id') tenant_id: string):Promise<Sentence[]>{
+async findAll():Promise<Sentence[]>{
 return this.sentencesServices.getAllSentences();
+}
+
+@Get('daily')
+async getSentenceOfTheDay():Promise<Sentence>{
+  return this.sentencesServices.getSentenceOfTheDay();
+}
+
+@Post('bulk')
+async createMany(@Body() createSentenceDtos:CreateSentenceDto[]):Promise<Sentence[]>{
+  return this.sentencesServices.createMany(createSentenceDtos);
+}
+
+@Post('single')
+async createOne(@Body() createSentenceDto: CreateSentenceDto):Promise<Sentence>{
+  return this.sentencesServices.createOne(createSentenceDto);
+}
+
+@Delete('bulk')
+async deleteMany():Promise<void>{
+  return this.sentencesServices.deleteAllSentences();
+}
+
+@Delete(':id')
+async deleteOne(@Param('id') id: string):Promise<void>{
+  return this.sentencesServices.deleteById(id);
+}
+
+@Put(':id/move-up')
+async moveUp(@Param('id') id: string):Promise<void>{
+  return this.sentencesServices.moveUp(id);
+}
+
+@Put(':id/move-down')
+async moveDown(@Param('id') id: string):Promise<void>{
+  return this.sentencesServices.moveDown(id);
 }
 
 }
